9d6ec6b636d9d39c320de94b260bc5fca537b264
hojinchang <72773536+hojinchang@users.noreply.github.com>
2023-11-21 15:06:03 -0800

    Refractor code into game controller class

--- scripts/script.js
+++ scripts/script.js
@@ -3,64 +3,85 @@
 /* ********************************************
               Character Movement 
 *********************************************** */
-const character = document.querySelector(".character-container");
-const characterIcon = document.querySelector(".character-icon");
-const characterHitbox = document.querySelector(".character-hitbox");
-const gameBoard = document.querySelector(".game-board");
+// const character = document.querySelector(".character-container");
+// const characterIcon = document.querySelector(".character-icon");
+// const characterHitbox = document.querySelector(".character-hitbox");
+// const gameBoard = document.querySelector(".game-board");
 
 
-const characterWidth = character.clientWidth;
-const characterHeight = character.clientHeight;
-const boardWidth = gameBoard.clientWidth;
-const boardHeight = gameBoard.clientHeight;
+// const characterWidth = character.clientWidth;
+// const characterHeight = character.clientHeight;
+// const boardWidth = gameBoard.clientWidth;
+// const boardHeight = gameBoard.clientHeight;
 
 
-const runImages = [];
-function initGame() {
-    function _placeCharacter() {
-        const xPosition = (boardWidth / 2) - (characterWidth / 2);
-        const yPosition = boardHeight - characterHeight;
+// const runImages = [];
+// function initGame() {
+//     function _placeCharacter() {
+//         const xPosition = (boardWidth / 2) - (characterWidth / 2);
+//         const yPosition = boardHeight - characterHeight;
 
-        character.style.left = `${xPosition}px`;
-        character.style.top = `${yPosition}px`;
-    }
+//         character.style.left = `${xPosition}px`;
+//         character.style.top = `${yPosition}px`;
+//     }
 
-    function _getRunImages() {
-        for (let i = 0; i < 8; i++) {
-            const image = new Image();
-            image.src = `../assets/character/Run-${i+1}.png`;
-            runImages.push(image);
-        }
-    }
+//     function _getRunImages() {
+//         for (let i = 0; i < 8; i++) {
+//             const image = new Image();
+//             image.src = `../assets/character/Run-${i+1}.png`;
+//             runImages.push(image);
+//         }
+//     }
 
-    _placeCharacter();
-    _getRunImages();
-}
+//     _placeCharacter();
+//     _getRunImages();
+// }
 
 
 
 class Robot {
-    constructor(idleImgPath, characterWidth, characterHeight) {
-        this.idleState = idleImgPath;
+    constructor(characterWidth, 
+                characterHeight, 
+                character, 
+                characterIcon, 
+                boardWidth, 
+                boardHeight) {
+
+        this.width = characterWidth;
+        this.height = characterHeight;
+        this.character = character;
+        this.characterIcon = characterIcon;
+        this.boardWidth = boardWidth;
+        this.boardHeight = boardHeight;
+
+        this.idleState = "../assets/character/Idle-1.png";
         this.isRunning = false;
         this.animationFrame;
         this.runIdx = 0;
         this.direction;
 
-        this.width = characterWidth;
-        this.height = characterHeight;
-
+        this.runImages = [];
+        this._getRunImages();
         this._runAnimation = this._runAnimation.bind(this);
     }
+
+    _getRunImages() {
+        for (let i = 0; i < 8; i++) {
+            const image = new Image();
+            image.src = `../assets/character/Run-${i+1}.png`;
+            this.runImages.push(image);
+        }
+    }
+
 
     /* 
         Cycle through runImages array to animate running.
         Move the character with a step of 2px when running
     */
     _runAnimation() {
-        characterIcon.src = runImages[this.runIdx].src;
+        this.characterIcon.src = this.runImages[this.runIdx].src;
         
-        let xPosition = parseInt(character.style.left);   // Current xPosition
+        let xPosition = parseInt(this.character.style.left);   // Current xPosition
         const step = 2;
 
         // Update the xPosition depending on if moving left or right
@@ -68,16 +89,16 @@ class Robot {
                                       xPosition = xPosition + step;
 
         // Set xPosition limits to be the edges of the game board
-        if ((xPosition + this.width) >= boardWidth) {
-            xPosition = boardWidth - this.width;
+        if ((xPosition + this.width) >= this.boardWidth) {
+            xPosition = this.boardWidth - this.width;
         } else if (xPosition <= 0) {
             xPosition = 0;
         }
 
-        character.style.left = `${xPosition}px`;
+        this.character.style.left = `${xPosition}px`;
 
         // Update the run image index and reset back to 0
-        this.runIdx = (this.runIdx + 1) % runImages.length;
+        this.runIdx = (this.runIdx + 1) % this.runImages.length;
 
         // Rerun the animation frame because it only runs once
         this.animationFrame = requestAnimationFrame(() => this._runAnimation());
@@ -89,8 +110,8 @@ class Robot {
             this.isRunning = true;
             this.direction = direction;
 
-            (this.direction === "left") ? character.classList.add("flip-character") : 
-                                          character.classList.remove("flip-character");
+            (this.direction === "left") ? this.character.classList.add("flip-character") : 
+                                          this.character.classList.remove("flip-character");
 
             this.animationFrame = requestAnimationFrame(() => this._runAnimation());
         }
@@ -100,26 +121,26 @@ class Robot {
     stopRunning() {
         cancelAnimationFrame(this.animationFrame);
         this.isRunning = false;
-        characterIcon.src = this.idleState;
+        this.characterIcon.src = this.idleState;
     }
 }
 
-const idleImagePath = "../assets/character/Idle-1.png";
-const robot = new Robot(idleImagePath, characterWidth, characterHeight);
+// const idleImagePath = "../assets/character/Idle-1.png";
+// const robot = new Robot(idleImagePath, characterWidth, characterHeight);
 
-document.addEventListener("keydown", (e) => {
-    if (e.key === "ArrowRight") {
-        robot.run("right");
-    } else if (e.key === "ArrowLeft") {
-        robot.run("left");
-    }
-})
+// document.addEventListener("keydown", (e) => {
+//     if (e.key === "ArrowRight") {
+//         robot.run("right");
+//     } else if (e.key === "ArrowLeft") {
+//         robot.run("left");
+//     }
+// })
 
-document.addEventListener("keyup", (e) => {
-    if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
-        robot.stopRunning();
-    }
-})
+// document.addEventListener("keyup", (e) => {
+//     if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+//         robot.stopRunning();
+//     }
+// })
 
 /* ********************************************
               Character Movement 
@@ -141,11 +162,11 @@ class Ball {
         this.yVelocity = 0;  // Vertical velocity
         this.bounceHeight = 350;  // Bounce height of balls in pixels
 
-        this.ball = this.createBall();
+        this.ball = this._createBall();
         this.bounce = this.bounce.bind(this);
     }
 
-    createBall() {
+    _createBall() {
         const ball = document.createElement("div");
         ball.classList.add("planet-container");
         const ballIcon = document.createElement("img");
@@ -189,9 +210,125 @@ class Ball {
     
         requestAnimationFrame(this.bounce);
     }
+
+    delete() {
+        gameBoard.removeChild(this.ball);
+    }
 }
 
-let source = "../assets/planets/planet04.png";
-const newBallObject = new Ball(source, 75, 75);
-newBallObject.bounce();
-initGame();
+
+
+
+class GameController {
+    constructor(devmode = false) {
+        this.devmode = devmode;
+
+        this.elements = {};
+        this._getElements();
+
+        this.ballImages = [];
+        this._getBallImages();
+
+        this.characterWidth = this.elements.character.clientWidth;
+        this.characterHeight = this.elements.character.clientHeight;
+        this.boardWidth = this.elements.gameBoard.clientWidth;
+        this.boardHeight = this.elements.gameBoard.clientHeight;
+        this._placeCharacter(this.boardWidth, this.boardHeight, this.characterWidth, this.characterHeight);
+    
+        this.robot = new Robot(this.characterWidth, this.characterHeight, this.elements.character, this.elements.characterIcon, this.boardWidth, this.boardHeight);
+
+        this._setUpEventListeners();
+
+
+        if (this.devmode) this._devmode();
+    }
+
+    _getElements() {
+        this.elements.introScreen = document.querySelector(".intro-screen");
+        this.elements.startGameBtn = document.querySelector(".start-game-btn");
+        this.elements.gameBoard = document.querySelector(".game-board");
+        this.elements.character = document.querySelector(".character-container");
+        this.elements.characterIcon = document.querySelector(".character-icon");
+    }
+
+    _setUpEventListeners() {
+        document.addEventListener("keydown", (e) => {
+            if (e.key === "ArrowRight") {
+                this.robot.run("right");
+            } else if (e.key === "ArrowLeft") {
+                this.robot.run("left");
+            }
+        })
+
+        document.addEventListener("keyup", (e) => {
+            if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+                this.robot.stopRunning();
+            }
+        })
+
+        
+        if (!this.devmode) {
+            this.elements.startGameBtn.addEventListener("click", () => {this._startGame()});
+            this.elements.introScreen.addEventListener("transitionend", () => {this._startGame(true)});
+        }
+        
+    }
+
+    _getBallImages() {
+        for (let i = 0; i < 10; i++) {
+            const image = new Image();
+            image.src = `../assets/planets/planet0${i}.png`;
+
+            this.ballImages.push(image);
+        }
+    }
+
+    _startGame(displayGameBoard = false) {
+        if (!displayGameBoard) {
+            this.elements.introScreen.classList.add("fade-out");
+        } else {
+            console.log("YESSIR")
+            this.elements.introScreen.classList.add("hide");
+            this.elements.gameBoard.classList.add("fade-in");
+        }
+    }
+
+    _placeCharacter(boardWidth, boardHeight, characterWidth, characterHeight) {
+        const xPosition = (boardWidth / 2) - (characterWidth / 2);
+        const yPosition = boardHeight - characterHeight;
+
+        this.elements.character.style.left = `${xPosition}px`;
+        this.elements.character.style.top = `${yPosition}px`;
+    }
+
+
+    _devmode() {
+        console.log("Dev Mode")
+        this.elements.introScreen.classList.add("hide");
+        this.elements.gameBoard.style.opacity = 100;
+        this.elements.gameBoard.style.transition = null;
+    }
+
+    
+}
+
+
+// const idleImagePath = "../assets/character/Idle-1.png";
+// const robot = new Robot(idleImagePath, characterWidth, characterHeight);
+
+// document.addEventListener("keydown", (e) => {
+//     if (e.key === "ArrowRight") {
+//         robot.run("right");
+//     } else if (e.key === "ArrowLeft") {
+//         robot.run("left");
+//     }
+// })
+
+// document.addEventListener("keyup", (e) => {
+//     if (e.key === "ArrowRight" || e.key === "ArrowLeft") {
+//         robot.stopRunning();
+//     }
+// })
+
+const game = new GameController(true);
+// const game = new GameController(false);
