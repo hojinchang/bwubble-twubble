7b72094d7873f6bb0d22c0ee1fd6302381c7e74b
hojinchang <hojinchang1998@gmail.com>
2023-11-26 14:00:18 -0800

    Refractor JS code into separate files

---
+++ scripts/ball.js
@@ -0,0 +1,123 @@
+/* ********************************************
+                    Ball 
+*********************************************** */
+const gravity = 0.05;  // Gravity constant
+class Ball {
+    constructor(
+        ballElement,
+        id,
+        xVelocity, 
+        yVelocity, 
+        bounceHeight,
+        gameBoardElement,
+    ) {
+        this.id = id;
+        this.ballElement = ballElement;  // ball DOM element
+        this.gameBoardElement = gameBoardElement;   // gameboard DOM element
+        this.width = this.ballElement.clientWidth;   // ball width
+        this.height = this.ballElement.clientHeight;  // ball height
+        this.xPosition = parseInt(this.ballElement.style.left);  // Horizontal position
+        this.yPosition = parseInt(this.ballElement.style.top);   // Vertical position
+
+        this.xVelocity = xVelocity;  // Horizontal velocity
+        this.yVelocity = yVelocity;  // Vertical velocity
+        this.bounceHeight = bounceHeight;  // Bounce height of balls in pixels
+        this.boardWidth = this.gameBoardElement.clientWidth;
+        this.boardHeight = this.gameBoardElement.clientHeight;
+
+        this.bounceAnimationFrame;
+        this.isDeleted = false;
+
+        this._onPositionChange;
+        this.bounce = this.bounce.bind(this);  // bind 'this' instance to be the ball object, 'this' loses pointer to ball object during animation frame
+    }
+
+    // Ball position tracking code from chatGPT. 
+    // set up a callback function to track the x/y position of the ball in the GameController class
+    set onPositionChangeCallback(callbackFunction) {
+        this._onPositionChange = callbackFunction;
+    }
+
+    bounce() {
+        if (this.isDeleted) return;
+    
+
+        // Ball Drop
+        this.yVelocity += gravity;  // a = dy/dt  =>  dy = a*dt  =>  dy_f - dy_i = a*dt  =>  dy_f = a*dt + dy_i  =>  where dt = each animation frame
+        this.yPosition += this.yVelocity;  // v = dx/dt  =>  dx = v*dt  =>  dx_f - dx_i = v*dt  =>  dx_f = v*dt + dx_i  =>  where dt = each animation frame
+        this.xPosition += this.xVelocity;
+        
+        // Bounce
+        if (this.yPosition > (this.boardHeight - this.height)) {
+            /*  To bring a ball back to a specified height, we must calculate the velocity required to
+                bounce the ball back to the height. Assuming elastic collision with no losses, the velocity 
+                down = velocity up. Thus, the final velocity of a ball as its dropped from a specified height 
+                is equal to the inital velocity of a ball as it collides with the floor and bounces back up.
+    
+                vf^2 = vi^2 + 2ad    // Assume that the ball is dropped with an initial velocity of 0
+                vf^2 = 2ad
+                vf = sqrt(2ad)
+            */
+            this.yVelocity = -Math.sqrt(2 * gravity * this.bounceHeight);  // Negative sign because in this context, down is positive and up is negative
+        }
+        
+        // Switch bounce direction if it hits the side walls
+        if (this.xPosition > (this.boardWidth - this.width) || this.xPosition < 0) {
+            this.xVelocity *= -1;
+        }
+    
+        this.ballElement.style.top = `${this.yPosition}px`;
+        this.ballElement.style.left = `${this.xPosition}px`;
+
+        if (this._onPositionChange) {   //  if callback function is set, execute it
+            this._onPositionChange();
+        }
+        
+        this.bounceAnimationFrame = requestAnimationFrame(() => this.bounce());
+    }
+
+    delete() {
+        this.isDeleted = true;
+        cancelAnimationFrame(this.bounceAnimationFrame);
+        this.bounceAnimationFrame = null;
+        this._onPositionChange = null;  // Stop callback function
+        this.ballElement.remove();  // Remove ball element from DOM
+    }
+}
+
+// Object containing the sizes and bounce heights of the different balls
+const ballSizes = {
+    ball1: {
+        width: 20,
+        height: 20,
+        bounceHeight: 115,
+        id: 1,
+    },
+    ball2: {
+        width: 32,
+        height: 32,
+        bounceHeight: 175,
+        id: 2,
+    },
+    ball3: {
+        width: 60,
+        height: 60,
+        bounceHeight: 250,
+        id: 3,
+    },
+    ball4: {
+        width: 90,
+        height: 90,
+        bounceHeight: 300,
+        id: 4,
+    },
+    ball5: {
+        width: 130,
+        height: 130,
+        bounceHeight: 350,
+        id: 5,
+    }
+}
+/* ********************************************
+                    Ball 
+*********************************************** */
---
+++ scripts/robot.js
@@ -0,0 +1,168 @@
+"use-strict"
+
+/* ********************************************
+                     Robot
+*********************************************** */
+class Robot {
+    constructor(
+        characterElement, 
+        characterIcon, 
+        gameBoardElement,
+    ) {
+        this.lives = 3;
+        this.characterElement = characterElement;
+        this.characterIcon = characterIcon;
+        this.gameBoardElement = gameBoardElement;
+        this.width = this.characterElement.clientWidth;
+        this.height = this.characterElement.clientHeight;
+        this.boardWidth = this.gameBoardElement.clientWidth;
+        this.boardHeight = this.gameBoardElement.clientHeight;
+
+        this.idleState = "../assets/character/Idle-1.png";
+        this.isRunning = false;
+        this.runAnimationFrame;
+        this.runIdx = 0;
+        this.direction;
+        this.xPosition;
+
+        this.laserObject;
+        this.isLaserActive;
+
+        this.runImages = this._getRunImages();
+        this._runAnimation = this._runAnimation.bind(this);   // bind this instance to be the robot object, this loses pointer to robot object during animation frame
+        this._laserAnimation = this._laserAnimation.bind(this);   // bind this instance to be the robot object, this loses pointer to robot object during animation frame
+    }
+
+    _getRunImages() {
+        const runImages = [];
+        for (let i = 0; i < 8; i++) {
+            const image = new Image();
+            image.src = `../assets/character/Run-${i+1}.png`;
+            runImages.push(image);
+        }
+        return runImages;
+    }
+
+    /* 
+        Cycle through runImages array to animate running.
+        Move the character with a step of 2px when running
+    */
+    _runAnimation() {
+        this.characterIcon.src = this.runImages[this.runIdx].src;
+        
+        this.xPosition = parseInt(this.characterElement.style.left);   // Current xPosition
+        const step = 2;
+
+        // Update the xPosition depending on if moving left or right
+        (this.direction === "left") 
+            ? this.xPosition = this.xPosition - step 
+            : this.xPosition = this.xPosition + step;
+
+        // Set xPosition limits to be the edges of the game board
+        if ((this.xPosition + this.width) >= this.boardWidth) {
+            this.xPosition = this.boardWidth - this.width;
+        } else if (this.xPosition <= 0) {
+            this.xPosition = 0;
+        }
+
+        this.characterElement.style.left = `${this.xPosition}px`;
+
+        // Update the run image index and reset back to 0
+        this.runIdx = (this.runIdx + 1) % this.runImages.length;
+
+        // Rerun the animation frame because it only runs once
+        this.runAnimationFrame = requestAnimationFrame(() => this._runAnimation());
+    }
+
+    // Run method
+    run(direction) {
+        if (!this.isRunning) {
+            this.isRunning = true;
+            this.direction = direction;
+
+            (this.direction === "left") 
+                ? this.characterElement.classList.add("flip-character") 
+                : this.characterElement.classList.remove("flip-character");
+
+            this._runAnimation();
+        }
+    }
+
+    _laserAnimation(yLaserStart) {
+        // this.isLaserActive is set false in the GameController when a collision between the laser and ball is detected
+        // Break out of the laser animation when collision occurs
+        if (!this.isLaserActive) return;
+        
+
+        // Increase the height of the laser object's property and laser DOM element
+        const step = 4;
+        this.laserObject.height += step;
+        this.laserObject.laserElement.style.height = `${this.laserObject.height}px`;
+
+        /*
+            Since the origin is the top left corner of the board, with positive y direction downwards, 
+            increasing the height increases the lasers height in the downward direction. The top offset
+            is changed to counteract the laser's growth downward.
+        */
+        const laserOffset = yLaserStart - this.laserObject.height;
+        this.laserObject.laserElement.style.top = `${laserOffset}px`
+        
+        /*
+            If the laser's height reaches the top of the screen, stop the laser animation and delete the laser
+            from the DOM and its instance.
+            Else, continue the laser animation.
+        */
+        if ((this.laserObject.height + this.height/2) >= this.boardHeight) {    // Since the laser starts from the center of the robot, half of the character height must be added to the laser height to "reach" the top of the gameboard
+            this.laserAnimationFrame = cancelAnimationFrame(this.laserAnimationFrame);
+            this.isLaserActive = false;
+
+            this.laserObject.delete();   // Delete the laser object and DOM element
+            this.laserObject = null;   // Remove laserObject from robot class
+        } else {
+            this.laserAnimationFrame = requestAnimationFrame(() => this._laserAnimation(yLaserStart));
+        }
+    }
+
+    // Shoot method
+    shoot(laser) {
+        // Only shoot if there is no lasers active
+        if (!this.isLaserActive) {
+            this.isLaserActive = true;
+            this.laserObject = laser;
+ 
+            const yLaserStart = this.boardHeight - this.height/2;   // Make laser start at middle of character height 
+            this._laserAnimation(yLaserStart);
+        }
+    }
+    
+    // Stop running method
+    stopRunning() {
+        this.runAnimationFrame = cancelAnimationFrame(this.runAnimationFrame);
+        this.isRunning = false;
+        this.characterIcon.src = this.idleState;
+    }
+}
+/* ********************************************
+                     Robot
+*********************************************** */
+
+
+/* ********************************************
+                     Laser
+*********************************************** */
+class Laser {
+    constructor(
+        laserElement
+    ) {
+        this.laserElement = laserElement;
+        this.height = 0;
+        this.width = parseInt(this.laserElement.style.width);
+    }
+
+    delete() {
+        this.laserElement.remove();
+    }
+}
+/* ********************************************
+                     Laser
+*********************************************** */
